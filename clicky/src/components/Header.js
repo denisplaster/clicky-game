2d2b3c8a4abc78bcffd303fe3f1fcc45d32bd574
DENIS PLASTER <denjeo@gmail.com>
2018-11-20 10:25:33 -0600

    Updates

--- clicky/src/components/Header.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-//stateless component
-const Header = props => <div className="wrapper">{props.children}</div>;
-
-export default Header;
---
+++ precache-manifest.3450562cf61fa2b61692961e5ee9457a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "32ce236cfdbae812f58e",
+    "url": "/clicky-game/static/js/main.32ce236c.chunk.js"
+  },
+  {
+    "revision": "8fdf2345b7a4e11badcc",
+    "url": "/clicky-game/static/js/1.8fdf2345.chunk.js"
+  },
+  {
+    "revision": "32ce236cfdbae812f58e",
+    "url": "/clicky-game/static/css/main.6c754f4a.chunk.css"
+  },
+  {
+    "revision": "3b78f895679d00a6947cf22e3cce884a",
+    "url": "/clicky-game/index.html"
+  }
+];
